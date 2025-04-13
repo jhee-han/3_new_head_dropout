4b749229216b31d55266e38e2f70792536203a30
juhee <138518729+jhee-han@users.noreply.github.com>
2025-04-12 22:06:35 -0700

    film

--- model.py
+++ model.py
@@ -18,12 +18,12 @@ class PixelCNNLayer_up(nn.Module):
                                         resnet_nonlinearity, skip_connection=1,film=film, embedding_dim=embedding_dim)
                                             for _ in range(nr_resnet)])
 
-    def forward(self, u, ul,class_embedding):
+    def forward(self, u, ul,class_embed_vec):
         u_list, ul_list = [], []
 
         for i in range(self.nr_resnet):
-            u  = self.u_stream[i](u, class_embed = class_embedding)
-            ul = self.ul_stream[i](ul, a=u + class_embedding, class_embed=class_embedding) #ul_stream은 gated_resnet의 instance이므로 gasted_resnet의 forward가 실행됨
+            u  = self.u_stream[i](u, class_embed = class_embed_vec)
+            ul = self.ul_stream[i](ul, a=u + class_embed_vec[:, :, None, None], class_embed=class_embed_vec) #ul_stream은 gated_resnet의 instance이므로 gasted_resnet의 forward가 실행됨
             u_list  += [u]
             ul_list += [ul]
 
@@ -105,8 +105,9 @@ class PixelCNN(nn.Module):
 
     def forward(self, x,class_labels, sample=False):
         # similar as done in the tf repo :
-        class_embedding =self.embedding(class_labels)  # (B, embedding_dim)
-        class_embedding = class_embedding.view(class_embedding.size(0),class_embedding.size(1),1,1) # (B, embedding_dim,1,1)
+        class_embed_vec =self.embedding(class_labels)  # (B, embedding_dim)
+        # class_embedding = class_embedding.view(class_embedding.size(0),class_embedding.size(1),1,1) # (B, embedding_dim,1,1)
+        class_embed_map = class_embed_vec[:, :, None, None]
 
         if self.init_padding is not sample:
             xs = [int(y) for y in x.size()]
@@ -125,7 +126,7 @@ class PixelCNN(nn.Module):
         ul_list = [self.ul_init[0](x) + self.ul_init[1](x)] #초기 feature map생성
         for i in range(3):
             # resnet block
-            u_out, ul_out = self.up_layers[i](u_list[-1], ul_list[-1],class_embedding)
+            u_out, ul_out = self.up_layers[i](u_list[-1], ul_list[-1],class_embed_vec)
             u_list  += u_out
             ul_list += ul_out
 
@@ -140,7 +141,7 @@ class PixelCNN(nn.Module):
 
         for i in range(3):
             # resnet block
-            u, ul = self.down_layers[i](u, ul, u_list, ul_list,class_embedding)
+            u, ul = self.down_layers[i](u, ul, u_list, ul_list,class_embed_vec)
 
             # upscale (only twice)
             if i != 2 :
