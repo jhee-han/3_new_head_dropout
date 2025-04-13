7c0ffe7360cc497fe7bbb8d64bdf0b0d6c2f4769
juhee <138518729+jhee-han@users.noreply.github.com>
2025-04-12 22:11:11 -0700

    embedding_dim=40

--- model.py
+++ model.py
@@ -55,7 +55,7 @@ class PixelCNNLayer_down(nn.Module):
 
 class PixelCNN(nn.Module):
     def __init__(self, nr_resnet=5, nr_filters=80, nr_logistic_mix=10,
-                    resnet_nonlinearity='concat_elu', input_channels=3, num_classes=4, embedding_dim=80, film=True):
+                    resnet_nonlinearity='concat_elu', input_channels=3, num_classes=4, embedding_dim=40, film=True):
         super(PixelCNN, self).__init__()
         if resnet_nonlinearity == 'concat_elu' :
             self.resnet_nonlinearity = lambda x : concat_elu(x)
